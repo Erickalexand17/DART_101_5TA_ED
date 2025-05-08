2390d6faff3fcd361036b5b47941ae7e0d80d0e8
Erickalexand17 <erickalexand@gmail.com>
2025-05-07 20:12:43 -0500

    uno

--- TAREAS/01_string_numeros.dart
+++ TAREAS/01_string_numeros.dart
@@ -3,3 +3,9 @@
   // 2.- Usar el double.parse pero en lugar de guardar "10.1" ("Pepito")
   // 3.- Que sucede
   // 4.- Como puedo resolver eso?
+
+  void main(){
+      String Nombre="Alexander";
+
+
+  }
