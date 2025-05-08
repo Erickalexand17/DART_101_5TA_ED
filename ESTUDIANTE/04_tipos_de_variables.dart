71b99e33ec6832cc8df3066ce1a3e0636f11cc90
Erickalexand17 <erickalexand@gmail.com>
2025-05-07 21:09:31 -0500

    dos

--- ESTUDIANTE/04_tipos_de_variables.dart
+++ ESTUDIANTE/04_tipos_de_variables.dart
@@ -21,6 +21,26 @@ void main(){
   // 2.- Usar el double.parse pero en lugar de guardar "10.1" ("Pepito")
   // 3.- Que sucede
   // 4.- Como puedo resolver eso?
+  
+
+  //Listas
+  List<String> compras=["leche","arroz","azucar","panes","pan"];
+  List<double> compras2=[1.25,3.25,1,3,2];
+  //Num toma valores int y double
+
+
+  String n1="Erick";
+  double d1=2.3;
+  int in1=2;
+
+  List<dynamic> total=[n1,d1,in1];
+  print(total);
+
+  total.add("yo");
+  print(total);
 
   
+  //tarea 2.- Imprimir el ultimo valor de la lista
+  int ope=total.length;
+  print(total.length);
 }
