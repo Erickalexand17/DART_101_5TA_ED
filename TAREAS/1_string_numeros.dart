208aa7bb7a66250733eb25bd97cecc97d73e1916
Erickalexand17 <erickalexand@gmail.com>
2025-05-07 19:38:36 -0500

    variables

---
+++ TAREAS/1_string_numeros.dart
@@ -0,0 +1,14 @@
+  //double.parse
+  // 1.- Consola "Mi nombre es Variable y tengo Variable"
+  // 2.- Usar el double.parse pero en lugar de guardar "10.1" ("Pepito")
+  // 3.- Que sucede
+  // 4.- Como puedo resolver eso?
+
+  void main(){
+    String nombre="Erick Alexander";
+    int edad=36;
+
+    print("Mi nombre es $nombre y tengo $edad años");
+
+
+  }
