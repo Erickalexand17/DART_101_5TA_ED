777155053f5b32a236abf3b4eee7c90f90bdd63a
Erickalexand17 <erickalexand@gmail.com>
2025-05-08 20:50:00 -0500

    uno

---
+++ TAREAS/3_mapas.dart
@@ -0,0 +1,26 @@
+// Tarea 3 
+  // final Map<String, String> restaurante3 = {
+  //   "nombre" : "Remoto",
+  //   "bebida": "hamburguresas",
+  //   "principal": "hot dog",
+  //   "fuerte": "Papas fritas",
+  // };
+  // De los mapas que nos creamos ustedes van a separlos, sus nuevas claves deben ser:
+  // "nombre" - String
+  // "menu" - Map<String, String>
+  // { 
+  // restaurante1 : {
+  //                  nombre: "Eliza",
+  //                  menu: {
+  //                            "bebida": "lo que sea",
+  //                            "fuerte": "lo que sea",
+  //                            "principal": "lo que sea"
+  //                         }  
+  //                 }
+  // }
+
+  // Como puedo acceder a los valores
+  // Imprimir El restaurante Eliza tiene un plato fuerte llamado sanduche
+  //Ingresar a buscar en los mapas
+  //print(miCadena["restaurante1"]);
+  //print(miCadena["restaurante1"]?["fuerte"]);
