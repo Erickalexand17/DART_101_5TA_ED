49333049deb8c5a490af498470dc420e65f32aa4
Erickalexand17 <erickalexand@gmail.com>
2025-05-08 19:16:44 -0500

    uno

--- TAREAS/01_string_numeros.dart
+++ TAREAS/01_string_numeros.dart
@@ -4,15 +4,23 @@ void main(){
       int edad=36;
       print("Mi nombre es $Nombre y tengo $edad");
 
-  String valorstring = "Pepito"; 
+  String valorstring = "10.2"; 
 
   final valor = double.tryParse(valorstring);
 
-  if (valor != null) {
+  print(valorstring);
+  print(valor);
+
+  print(valorstring.runtimeType);
+  print(valor.runtimeType);
+    
+    if (valor != null) {
     print("El número es: $valor");
   } else {
     print("No se pudo convertir '$valorstring' a double porque es String.");
   }
 
   
+  
+   
   }
