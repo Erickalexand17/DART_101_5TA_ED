df595c755b409b1fbd406d3fd2415ba58e57318f
Erickalexand17 <erickalexand@gmail.com>
2025-05-13 21:07:18 -0500

    uno

---
+++ ESTUDIANTE/08_if_else.dart
@@ -0,0 +1,27 @@
+void main() {
+  
+  final salario=500;
+  final setpoint=100;
+
+
+  if (salario >= setpoint){
+    print("Me compro celular");
+
+  }else{
+    print("No puedo comer");
+  }
+
+  // condicion ternaria
+  final resultado= salario >= setpoint ? print("Me compro celular") : print("No puedo comer");
+
+final edad=3
+;
+
+  if (edad >= 18){
+    print("mayor de edad");
+}else if (edad >= 4 && edad <= 17){
+    print("adolesecente");
+}else if (edad < 4 ){
+    print("niño");
+}
+}
---
+++ TAREAS/04_if_else.dart
@@ -0,0 +1,15 @@
+void main() {
+  final edad=20;
+
+  final mayoredad=18;
+  final adolesecente=17;
+  final nino=4;
+
+  if (edad >= mayoredad){
+    print("mayor de edad");
+}else if (edad >= nino && edad <= adolesecente){
+    print("adolesecente");
+}else if (edad < nino ){
+    print("niño");
+}
+}
