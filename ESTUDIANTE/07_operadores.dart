f45e02941a486b8d3c4f134be27c2d7f96573e1b
Erickalexand17 <erickalexand@gmail.com>
2025-05-13 20:24:13 -0500

    uno

--- ESTUDIANTE/07_operadores.dart
+++ ESTUDIANTE/07_operadores.dart
@@ -1,7 +1,38 @@
 void main() {
-  final valor1=1;
-  final valor2=2;
-  final suma= valor1+valor2;
+  final valor1=22;
+  final valor2=3;
+  final suma= valor1/valor2;
 
   print(suma);
+
+final igual = valor1==valor2;
+print(igual);
+
+final desigual=valor1!=valor2;
+print(desigual);
+
+final mayor= valor2>valor1;
+print(mayor);
+
+bool menos=valor2<valor1;
+print(menos);
+
+//Modulo resto de una division
+final modulo=valor1%valor2;
+print(modulo);
+
+final a=false, b=true;
+
+final valueand = a && b;
+print(valueand);
+
+final valueor = a || b;
+print(valueor);
+
+final valuenot=!b;
+print(valuenot);
+
+
+
+
 }
